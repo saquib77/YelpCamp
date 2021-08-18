7d997b139e72a1824744e9bb970d15c4c5e29de3
saquib77 <chand567khan@gmail.com>
2021-08-18 23:12:50 +0530

    minor tweaks

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const path = require('path');
 const ejsMate = require('ejs-mate');
+const fileUpload = require('express-fileupload');
 const methodOverride = require('method-override');
 const Campground = require("./models/campground");
 
@@ -17,9 +18,12 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database Connected");
 });
+
+app.use(fileUpload());
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
@@ -40,7 +44,7 @@ app.post('/campgrounds', async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
-})
+});
 
 app.get('/campgrounds/:id', async (req, res,) => {
     const campground = await Campground.findById(req.params.id)
--- seeds/index.js
+++ seeds/index.js
@@ -18,14 +18,17 @@ db.once("open", () => {
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
-
+const price = Math.floor(Math.random() * 20) + 10;
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/155011',
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi odio totam architecto voluptatem similique, mollitia quas dolore maiores pariatur ipsam libero provident perspiciatis delectus, atque nihil, corporis praesentium excepturi? Autem.',
+            price
         })
         await camp.save();
     }
